73683c2e6652bd18b08d99a9c4525f3698293bcc
Aditya <106978327+AdityaGautam2512@users.noreply.github.com>
2025-04-29 01:50:09 +0530

    Update inner_pages.js

--- Assets/js/inner_pages.js
+++ Assets/js/inner_pages.js
@@ -68,3 +68,24 @@ $(function () {
     });
 
 });
+
+document.getElementById('contact-form').addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const form = e.target;
+    const data = new FormData(form);
+    const response = await fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        alert('Thank you! Your message has been sent.');
+        form.reset();
+    } else {
+        alert('Oops! There was a problem submitting your form.');
+    }
+});
+
